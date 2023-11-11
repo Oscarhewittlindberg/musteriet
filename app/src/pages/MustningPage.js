fb53254c817162b11c8354a79145a07b57766af4
Oscar Lindberg <oscar.lindberg@voyado.com>
2023-11-11 15:20:59 +0100

    structure in place

---
+++ app/src/pages/MustningPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Grid, Header } from 'semantic-ui-react'
+
+const MustningPage = () => (
+  <Grid divided='vertically'>
+    <Grid.Row columns={2}>
+      <Grid.Column>
+        
+      </Grid.Column>
+      <Grid.Column>
+        <Header>Hej</Header>
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row columns={3}>
+      <Grid.Column>
+        hej
+      </Grid.Column>
+      <Grid.Column>
+        hej
+      </Grid.Column>
+      <Grid.Column>
+        hej
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
+
+export default MustningPage
